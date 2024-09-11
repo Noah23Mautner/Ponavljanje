2165612d46c8acdfa5e4259f67b84e54c33a7b74
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-09-11 09:29:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Ponavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj članova reda:");
+            double suma = 0;
+            double brojClanova = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 0; i < brojClanova; i++)
+            {
+                suma += 1 / Math.Pow(3, i + 1);
+            }
+
+            Console.WriteLine("Suma reda je: " + suma);
+            Console.ReadLine();
+
+        }
+    }
+}
